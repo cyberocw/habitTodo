966e8d3235c0a8a9bd35f7b0031cdb7c15f10d78
cyberocw <oppa1324>
2015-11-27 23:15:41 +0900

    timer complete

--- app/src/main/java/com/cyberocw/habittodosecretary/util/AlarmNotiActivity.java
+++ app/src/main/java/com/cyberocw/habittodosecretary/util/AlarmNotiActivity.java
@@ -1,11 +1,11 @@
 package com.cyberocw.habittodosecretary.util;
 
+import android.app.KeyguardManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.os.Vibrator;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
 
@@ -27,6 +27,14 @@ public class AlarmNotiActivity extends AppCompatActivity {
 		long[] pattern = {1000, 200, 1000, 2000, 1200};          // 진동, 무진동, 진동 무진동 숫으로 시간을 설정한다.
 		mVibe.vibrate(pattern, 0);                                         // 패턴을 지정하고 반복횟수를 지정
 		mVibe.vibrate(30000);                                                   //1초 동안 진동이 울린다.
+
+		PowerManager pm = (PowerManager) getApplicationContext().getSystemService(Context.POWER_SERVICE);
+		PowerManager.WakeLock wakeLock = pm.newWakeLock((PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP), "TAG");
+		wakeLock.acquire();
+
+		KeyguardManager keyguardManager = (KeyguardManager) getApplicationContext().getSystemService(Context.KEYGUARD_SERVICE);
+		KeyguardManager.KeyguardLock keyguardLock = keyguardManager.newKeyguardLock("TAG");
+		keyguardLock.disableKeyguard();
 
 		Button btnStop = (Button) findViewById(R.id.btnTimerStop);
 		btnStop.setOnClickListener(new View.OnClickListener() {
