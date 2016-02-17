2930c93c9920b1c1610fae6824cd3cb56b07e9f0
cyberocw <cyberocw@gmail.com>
2016-02-17 19:32:16 +0900

    재부팅 시 alarm 등록 수정

--- app/src/main/java/com/cyberocw/habittodosecretary/util/BootReceiver.java
+++ app/src/main/java/com/cyberocw/habittodosecretary/util/BootReceiver.java
@@ -3,6 +3,7 @@ package com.cyberocw.habittodosecretary.util;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 import com.cyberocw.habittodosecretary.Const;
 import com.cyberocw.habittodosecretary.alaram.AlarmDataManager;
